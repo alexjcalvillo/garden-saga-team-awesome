e64d88ad02cdb46de97ab5801ecdccbf6225e1ea
Alexander Calvillo <alexanderjcalvillo@gmail.com>
2020-07-29 16:56:36 -0500

    adds redux logger and redux sagas. creates GET call and displays on plantlist component

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
+import './PlantList.css';
 import { connect } from 'react-redux';
 
-const mapStateToProps = reduxState => ({
-    reduxState,
+const mapStateToProps = (store) => ({
+  plantList: store.plantList,
 });
 
 class PlantList extends Component {
-    componentDidMount() {
-        // use component did mount to dispatch an action to request the plantList from the API
-    }
+  componentDidMount() {
+    // use component did mount to dispatch an action to request the plantList from the API
+    this.props.dispatch({
+      type: 'GET_PLANTS',
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <h3>This is the plant list</h3>
-                <pre>{JSON.stringify(this.props.reduxState)}</pre>
-            </div>
-        );
-    }
+  render() {
+    console.log(this.props.plantList);
+    return (
+      <div>
+        <h3>This is the plant list</h3>
+        <pre>{JSON.stringify(this.props.reduxState)}</pre>
+        <ul className="list">
+          {this.props.plantList.map((item, index) => {
+            return <li key={item.id}>{item.name}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default connect(mapStateToProps)(PlantList);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-
 import App from './App';
 
-// this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { takeEvery, takeLatest, put } from 'redux-saga/effects';
 
-const plantList = (state = startingPlantArray, action) => {
+// IMPORT MIDDLEWARE
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import axios from 'axios';
+
+// sagas waiting for dispatch
+function* rootSaga() {
+  yield takeEvery('GET_PLANTS', getPlants);
+}
+
+function* getPlants() {
+  try {
+    const response = yield axios.get('/api/plant');
+    yield put({
+      type: 'SET_PLANTS',
+      payload: response.data,
+    });
+  } catch (err) {
+    console.log(`Yikes, didn't get that. ${err}`);
+  }
+}
+
+// SETUP saga/ADD saga
+const sagaMiddleware = createSagaMiddleware();
+
+// this startingPlantArray should eventually be removed
+// const startingPlantArray = [
+//   { id: 1, name: 'Rose' },
+//   { id: 2, name: 'Tulip' },
+//   { id: 3, name: 'Oak' },
+// ];
+
+const plantList = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_PLANT':
-      return [ ...state, action.payload ]
+    case 'SET_PLANTS':
+      return action.payload;
     default:
       return state;
   }
@@ -23,6 +49,15 @@ const plantList = (state = startingPlantArray, action) => {
 
 const store = createStore(
   combineReducers({ plantList }),
+  applyMiddleware(logger, sagaMiddleware)
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
+// to run sagas
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('react-root')
+);
