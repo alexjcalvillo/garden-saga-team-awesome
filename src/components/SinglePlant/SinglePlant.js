08ae8d7b9fe14b99474ea95a32a074c788f8c457
Kenneth Carter <kxccarter@gmail.com>
2020-07-31 11:57:25 -0500

    Conditionally renders inputs for EDIT MODE on SinglePlant.js

--- src/components/SinglePlant/SinglePlant.js
+++ src/components/SinglePlant/SinglePlant.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './SinglePlant.module.css';
 
+// Material UI ---
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Button } from '@material-ui/core';
+
 const mapStateToProps = (store) => ({
   plantDetails: store.plantDetails,
 });
 
 class SinglePlant extends Component {
   state = {
-    id: '',
+    editMode: false,
+    newPlant: {
+      name: '',
+      kingdom: '',
+      clade: '',
+      order: '',
+      family: '',
+      subfamily: '',
+      genus: '',
+    },
   };
 
   componentDidMount() {
@@ -22,41 +43,180 @@ class SinglePlant extends Component {
   }
 
   handleOnEditClick = () => {
-    this.props.dispatch({});
+    console.log(`let's edit so good`);
+    this.setState({ editMode: !this.state.editMode });
+  };
+
+  handleNameChange = (fieldKey) => (event) => {
+    console.log('event happended', this.state.newPlant);
+    this.setState({
+      newPlant: {
+        ...this.state.newPlant,
+        [fieldKey]: event.target.value,
+      },
+    });
+  };
+
+  addNewPlant = (event) => {
+    event.preventDefault();
+    this.props.dispatch({ type: 'ADD_PLANT', payload: this.state.newPlant });
+    this.setState({
+      newPlant: {
+        id: this.state.newPlant.id + 1,
+        name: '',
+        kingdom: '',
+        clade: '',
+        order: '',
+        family: '',
+        subfamily: '',
+        genus: '',
+      },
+    });
   };
 
   render() {
-    console.log(this.props.plantDetails);
-    console.log(this.props);
     return (
       <div className="container-admin">
         <h3>This is the single plant</h3>
-
-        <table className="list table-set table-border">
-          <thead className="table-head">
-            <tr>
-              <th>Name</th>
-              <th>Kingdom</th>
-              <th>Clade</th>
-              <th>Order</th>
-              <th>Family</th>
-              <th>Sub-Family</th>
-              <th>Genus</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr key={this.props.plantDetails.id}>
-              <td>{this.props.plantDetails.name}</td>
-              <td>{this.props.plantDetails.kingdom}</td>
-              <td>{this.props.plantDetails.clade}</td>
-              <td>{this.props.plantDetails.order}</td>
-              <td>{this.props.plantDetails.family}</td>
-              <td>{this.props.plantDetails.subfamily}</td>
-              <td>{this.props.plantDetails.genus}</td>
-              <button onClick={this.handleOnEditClick}>Edit</button>
-            </tr>
-          </tbody>
-        </table>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="right">Name</TableCell>
+                <TableCell align="right">Kingdom</TableCell>
+                <TableCell align="right">Clade</TableCell>
+                <TableCell align="right">Order</TableCell>
+                <TableCell align="right">Family</TableCell>
+                <TableCell align="right">SubFamily</TableCell>
+                <TableCell align="right">Genus</TableCell>
+                <TableCell align="right"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {!this.state.editMode ? (
+                <TableRow>
+                  <TableCell align="right">
+                    {this.props.plantDetails.name}
+                  </TableCell>
+                  <TableCell align="right">
+                    {this.props.plantDetails.kingdom}
+                  </TableCell>
+                  <TableCell align="right">
+                    {this.props.plantDetails.clade}
+                  </TableCell>
+                  <TableCell align="right">
+                    {this.props.plantDetails.order}
+                  </TableCell>
+                  <TableCell align="right">
+                    {this.props.plantDetails.family}
+                  </TableCell>
+                  <TableCell align="right">
+                    {this.props.plantDetails.subfamily}
+                  </TableCell>
+                  <TableCell align="right">
+                    {this.props.plantDetails.genus}
+                  </TableCell>
+                  <TableCell align="right">
+                    <Button
+                      variant="contained"
+                      onClick={this.handleOnEditClick}
+                    >
+                      Edit
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ) : (
+                <TableRow>
+                  <TableCell align="right">
+                    <TextField
+                      id="standard-basic"
+                      label="name"
+                      variant="outlined"
+                      type="text"
+                      name="name"
+                      value={this.state.newPlant.name}
+                      onChange={this.handleNameChange('name')}
+                    />
+                  </TableCell>
+                  <TableCell align="right">
+                    <TextField
+                      id="standard-basic"
+                      label="kingdom"
+                      variant="outlined"
+                      type="text"
+                      name="kingdom"
+                      value={this.state.newPlant.kingdom}
+                      onChange={this.handleNameChange('kingdom')}
+                    />
+                  </TableCell>
+                  <TableCell align="right">
+                    <TextField
+                      id="outlined-basic"
+                      variant="outlined"
+                      label="clade"
+                      type="text"
+                      name="clade"
+                      value={this.state.newPlant.clade}
+                      onChange={this.handleNameChange('clade')}
+                    />
+                  </TableCell>
+                  <TableCell align="right">
+                    <TextField
+                      id="outlined-basic"
+                      variant="outlined"
+                      label="order"
+                      type="text"
+                      name="order"
+                      value={this.state.newPlant.order}
+                      onChange={this.handleNameChange('order')}
+                    />
+                  </TableCell>
+                  <TableCell align="right">
+                    <TextField
+                      id="outlined-basic"
+                      variant="outlined"
+                      label="family"
+                      type="text"
+                      name="family"
+                      value={this.state.newPlant.family}
+                      onChange={this.handleNameChange('family')}
+                    />
+                  </TableCell>
+                  <TableCell align="right">
+                    <TextField
+                      id="outlined-basic"
+                      variant="outlined"
+                      label="subfamily"
+                      type="text"
+                      name="subfamily"
+                      value={this.state.newPlant.subfamily}
+                      onChange={this.handleNameChange('subfamily')}
+                    />
+                  </TableCell>
+                  <TableCell align="right">
+                    <TextField
+                      id="outlined-basic"
+                      variant="outlined"
+                      label="genus"
+                      type="text"
+                      name="genus"
+                      value={this.state.newPlant.genus}
+                      onChange={this.handleNameChange('genus')}
+                    />
+                  </TableCell>
+                  <TableCell align="right">
+                    <Button
+                      variant="contained"
+                      onClick={this.handleOnEditClick}
+                    >
+                      Save
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     );
   }
